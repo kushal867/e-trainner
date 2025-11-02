16957d2a39f0dac12daa585f7a58e8467a9200fc
Kushal <kushalsapkota577@gmail.com>
2025-11-02 17:08:46 +0545

    updated

--- home/views.py
+++ home/views.py
@@ -1,42 +1,43 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Gym, Product, GymReview
-from .forms import GymForm, ProductForm, GymReviewForm  
+from .models import Gym
+from .forms import GymForm
 
-
+# List all gyms
 def gym_list(request):
-    data = Gym.objects.all()
+    gyms = Gym.objects.all()
     context = {
-        "gyms": data
+        "gym":gyms
     }
     return render(request, "index.html", context)
 
+from django.shortcuts import render, redirect
+from .forms import GymForm
 
 def gym_create(request):
     if request.method == 'POST':
         form = GymForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('gym_list')  
+            return redirect('gym_list')  # use the URL pattern name, not the view
     else:
         form = GymForm()
 
-    return render(request, "gym_create.html", {"form": form})
+    return render(request, "gym_create.html", {'form': form})
 
 
 def gym_edit(request, id):
-    data = Gym( id=id)
+    gyms = Gym.objects.get(id=id)
+    form = GymForm(instance=gyms)
     if request.method == 'POST':
-        form = GymForm(request.POST, instance=id)
+        form = GymForm(instance=gyms)
         if form.is_valid():
             form.save()
-            return redirect('gym_list')
-    else:
-        form = GymForm(instance=id)
-
-    return render(request, "gym_edit.html", {"form": form})
-
+            return redirect(gym_list)
+        else:
+            form = GymForm(instance=gyms)
+            return render(request, "gym_edit.html", {"form":form})
 
 def gym_delete(request, id):
-   data = Gym(id=id)
-   data.delete()
-   return redirect('gym_list')
+    gyms = Gym.objects.get(id=id)
+    gyms.delete()
+    return redirect(gym_list)

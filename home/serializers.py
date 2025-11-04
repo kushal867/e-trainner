1decf42514162b3bb70100ced878061d8c66ade4
Kushal <kushalsapkota577@gmail.com>
2025-11-04 16:51:54 +0545

    feat:updated

--- home/serializers.py
+++ home/serializers.py
@@ -1,13 +1,18 @@
 from rest_framework import serializers
-from .models import Gym, Product
+from .models import Gym, Product, GymReview
 
 class GymSerializer(serializers.ModelSerializer):
     class Meta:
         model =  Gym
         fields = "__all__"
 
-class PorductSeralizer(serializers.ModelSerializer):
+class PorductSeraializer(serializers.ModelSerializer):
     class Meta:
         model = Product
+        fields = "__all__"
+
+class GymReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model =  Gym
         fields = "__all__"
         

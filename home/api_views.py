41121487f0354877d009c2101713226a5022d9b0
Kushal <kushalsapkota577@gmail.com>
2025-11-04 16:52:22 +0545

    feat:updated

--- home/api_views.py
+++ home/api_views.py
@@ -1,9 +1,17 @@
 from rest_framework import viewsets
-from .models import Gym
-from serializers import GymSerializer
+from .models import Gym, Product, GymReview
+from serializers import GymSerializer, PorductSeraializer, GymReviewSerializer
 
 class GymViewSet(viewsets.ModelViewSet):
     queryset = Gym.objects.all()
     serializer_class = GymSerializer
     
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = PorductSeraializer
+
+class Gymviewset(viewsets.ModelViewSet):
+    queryclass = GymReview.objects.all()
+    serializer_class = GymReviewSerializer
     

6a9917bab76011bc4922b618a56e4bf321741b12
Kushal <kushalsapkota577@gmail.com>
2025-11-05 17:09:42 +0545

    feat:Updated

--- home/serializers.py
+++ home/serializers.py
@@ -3,16 +3,15 @@ from .models import Gym, Product, GymReview
 
 class GymSerializer(serializers.ModelSerializer):
     class Meta:
-        model =  Gym
+        model = Gym
         fields = "__all__"
 
-class PorductSeraializer(serializers.ModelSerializer):
+class ProductSerializer(serializers.ModelSerializer):  # fixed typo
     class Meta:
         model = Product
         fields = "__all__"
 
 class GymReviewSerializer(serializers.ModelSerializer):
     class Meta:
-        model =  Gym
+        model = GymReview  # fixed model reference
         fields = "__all__"
-        

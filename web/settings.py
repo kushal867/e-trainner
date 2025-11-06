e6eaf714bff52ee494a011732f773a770ae86b41
Kushal <kushalsapkota577@gmail.com>
2025-11-06 17:12:59 +0545

    feat:updated in the logic

--- web/settings.py
+++ web/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'home',
     'rest_framework',
+    'user',
     
 ]
 
@@ -123,3 +124,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+

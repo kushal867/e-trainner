2db7a683788a69292401dcac75eaee1955403f33
Kushal Sapkota <139784426+kushal867@users.noreply.github.com>
2026-03-23 18:00:57 +0545

    Rename app and update URL patterns for user views

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import user_login, user_logout
+from . import views
 
-app_name = 'user'
+app_name = 'users'
 
 urlpatterns = [
-    path('', user_login, name='login'),
-    path('logout/', user_logout, name='logout'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
 ]

4566b194e775418ea0afaaf1ff9044c519895600
Kushal Sapkota <139784426+kushal867@users.noreply.github.com>
2026-03-23 17:57:50 +0545

    Add permissions and filters to viewsets

--- home/api_views.py
+++ home/api_views.py
@@ -1,15 +1,31 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions, filters
 from .models import Gym, Product, GymReview
 from .serializers import GymSerializer, ProductSerializer, GymReviewSerializer
+
 
 class GymViewSet(viewsets.ModelViewSet):
     queryset = Gym.objects.all()
     serializer_class = GymSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['name', 'location']
+    ordering_fields = ['name', 'created_at']
+
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['name', 'category']
+    ordering_fields = ['price', 'created_at']
+
 
 class GymReviewViewSet(viewsets.ModelViewSet):
     queryset = GymReview.objects.all()
     serializer_class = GymReviewSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    
+    def perform_create(self, serializer):
+        # Automatically assign logged-in user
+        serializer.save(user=self.request.user)
